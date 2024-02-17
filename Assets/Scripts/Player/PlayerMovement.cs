0861486546ab61f3b9fe3ba0ba60428495c78840
popoyo07 <piwedaa@gmail.com>
2024-02-17 01:52:54 -0500

    Added more empty objects and a script
    I added a script for the movement, its kind of broken right now because I need a ground already set up so the object of characters can move around. There are also like a ball I added and is just for testing. Please don't delete the actual objects, just replace or change the needed componets.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb; 
+    private BoxCollider2D coli;
+    private Animator anim;
+    private SpriteRenderer sprite;
+
+    [SerializeField] private LayerMask jumpableGround;
+
+    private float dirx = 0f; 
+
+    [SerializeField] private float moveSpeed = 7f; // place holder for speed and force
+    [SerializeField] private float jumpForce = 10f;
+
+    private enum MovementState { idl, running, junmping, falling } // here is making a custom value variable so I can track animatinos. Their position means 0, 1, 2, 3 and so on
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coli = GetComponent<BoxCollider2D>();
+       // anim = GetComponent<Animator>(); // conects with the animation components so I can use them in variables
+        // sprite = GetComponent<SpriteRenderer>(); 
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        dirx = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(dirx * moveSpeed, rb.velocity.y); //Movement in Direction 
+
+
+        //   || Input.GetKeyDown("'W' key")
+        if (Input.GetKeyDown("'W' key") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        // will add later "updateAnimation();"
+
+    }
+
+   /* private void updateAnimation()
+    {
+        MovementState state;
+
+        if (dirx > 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = false;
+        }
+        else if (dirx < 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = true;
+        }
+        else
+        {
+            state = MovementState.idl;
+        }
+
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.junmping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+        anim.SetInteger("State", (int)state);
+    } */
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coli.bounds.center,
+            coli.bounds.size,
+            0f, Vector2.down,
+            .1f,
+            jumpableGround); // reading so can the player can only jump when is touching the ground 
+    }
+}
